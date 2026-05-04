c45091f01f0c6089bb56421e7782e48deaf71591
Stalin Amagua <stalin.amagua@outlook.com>
2026-05-04 00:16:02 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# 01-typescript-intro
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
---
+++ src/topics/01-basic-types.ts
@@ -0,0 +1,11 @@
+const name: string = 'Jueltter';
+
+let healthPoints: number | string = 100;
+
+const isAlive: boolean = true;
+
+healthPoints = 'Full';
+
+console.log({ name, healthPoints, isAlive });
+
+export {};
