8abc138bbc21e3584bed7d6fd4c8ea729b0fd7a0
Stalin Amagua <stalin.amagua@outlook.com>
2026-05-05 00:39:53 -0500

    3-funciones-con-objetos-como-argumentos completed

--- src/topics/03-functions.ts
+++ src/topics/03-functions.ts
@@ -12,12 +12,35 @@ function multiply(first: number, second?: number, base: number = 2): number {
     return first * base;
 }
 
-const result = addNumbers( 1, 2 );
-const arrowResult = addNumbersArrow( 1, 2 );
-const multiplyResult = multiply( 5 );
+// const result = addNumbers( 1, 2 );
+// const arrowResult = addNumbersArrow( 1, 2 );
+// const multiplyResult = multiply( 5 );
 
-console.log({ result });
-console.log({ arrowResult });
-console.log({ multiplyResult });
+// console.log({ result });
+// console.log({ arrowResult });
+// console.log({ multiplyResult });
+
+interface Character {
+    name: string;
+    hp: number;
+    showHp: () => void;
+}
+
+
+const healCharacter = (character: Character, amount: number) => {
+    character.hp += amount;
+}
+
+const strider: Character = {
+    name: 'Strider',
+    hp: 100,
+    showHp() {
+        console.log(`HP: ${this.hp}`);
+    }
+}
+
+healCharacter(strider, 20);
+strider.showHp();
+
 
 export {};
