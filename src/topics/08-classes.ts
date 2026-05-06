45f30015fabc66d27ed72400cf9f28c9a9dabb1a
Stalin Amagua <stalin.amagua@outlook.com>
2026-05-05 21:04:06 -0500

    3-constructor-de-una-clase completed

--- src/topics/08-classes.ts
+++ src/topics/08-classes.ts
@@ -1,13 +1,17 @@
 export class Person {
-    public name: string;
-    private address: string;
+    // public name: string;
+    //private address: string;
 
-    constructor(name: string, address: string) {
-        this.name = name;
-        this.address = address;
+    constructor(
+        public name: string, 
+        private address: string = "Unknown") {
+        // this.name = name;
+        // this.address = address;
     }
 }
 
 const person1 = new Person("Alice", "123 Main St"); 
+const person2 = new Person("Bob");
 
 console.log(person1);
+console.log(person2);
